98c4b2e9101d7b7822864cf30389872d81d6a413
GitHubZJY <742155745@qq.com>
2015-10-25 23:51:23 +0800

    单人模式与入口选项

--- src/com/example/z2/AnimationTest.java
+++ src/com/example/z2/AnimationTest.java
@@ -1,14 +1,14 @@
-package com.example.z2;
+package com.example.test;
 
+import java.io.IOException;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
@@ -19,7 +19,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Display;
@@ -33,78 +33,146 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import com.example.z2.Gallery3DActivity;
 
-@SuppressLint("NewApi")
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+import com.example.test.Gallery3DActivity;
+
 public class AnimationTest extends Activity {
 	Handler handler;        //产生滑块的线程
-	Runnable task;
-	int speed=1200;
+	Runnable task,task1;
+	static int speed=1100;         //滑块滑动速度
 	int Width,Height;       //屏幕宽高
 	int colorrandom[] = {Color.BLACK,Color.WHITE};  //黑白随机数组
 	Random r;
-	int currentcolor;
-	int count=0;		//计算所按黑块的数量	
+	int currentcolor;              //Random产生的随机数
+	int count=0;		           //计算所按黑块的数量	
 	Button left,right;             //左列和右列
-	ObjectAnimator objleft,objright;       //左列的移动和右列的移动
+	//ObjectAnimator objleft,objright;      
 	Button start;                  //游戏启动
 	FrameLayout.LayoutParams params;     
 	TextView Viewcount;          //屏幕上方显示计数
+	Button[] bgroup;                //铺满整个屏幕的按钮数组
+	ObjectAnimator objgroup[];      //按钮对应动画数组
+	MediaPlayer mp3;
+	int groupIndex=0;               //判断按钮行数
+	int isPressed = 0;
+	ImageButton imagestart;
 	@Override
 	
 	protected void onCreate(Bundle savedInstanceState) {
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);   //设置取消安卓标题栏
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_animation_test);
-		left = (Button)this.findViewById(R.id.bbb);
-		start = (Button)this.findViewById(R.id.ccc);
-		right = (Button)this.findViewById(R.id.ddd);
+		//start = (Button)this.findViewById(R.id.ccc);
+		
+		imagestart = (ImageButton)this.findViewById(R.id.imagestart);
 		Display display = this.getWindowManager().getDefaultDisplay();
         Width = display.getWidth();
         Height = display.getHeight();
         Viewcount = new TextView(AnimationTest.this);
         params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
-        
+        bgroup = new Button[12];   //铺满界面的6行按钮
+        for(int i=0;i<12;i++){
+        	bgroup[i] = new Button(AnimationTest.this);
+        }
+        objgroup = new ObjectAnimator[12];    //12个按钮的动画
+        for(int j=0;j<12;j++){
+        	objgroup[j] = new ObjectAnimator();
+        }
         
         
         handler = new Handler();                          //定时产生滑块
         task = new Runnable() {  
             public void run() {   
                     handler.postDelayed(this,speed/4-speed/36);//设置延迟时间
-                    initBlock(left, right);
+                    groupBlock();
             }   
         };
         
+        task1 = new Runnable(){
+        	public void run(){
+        		mp3 = new MediaPlayer();
+				mp3 = MediaPlayer.create(AnimationTest.this,R.raw.rnl);
+				try {
+					mp3.prepare();
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				mp3.start();
+        	}
+        };
         
-		start.setX(Width/4);
-		start.setY(Height/2);
-		start.setWidth(Width/2);
-		start.setOnClickListener(new OnClickListener() {		
+        
+//		imagestart.setX(Width/4);
+//		imagestart.setY(Height/2);
+//		imagestart.setWidth(Width/2);
+		imagestart.setOnClickListener(new OnClickListener() {		
 			@Override
 			public void onClick(View v) {	
 				//initBlock(bbb,ddd);
+				 handler.post(task1);
 				 handler.post(task);//立即调用
 			}
 		});
 		
-		this.addContentView(Viewcount, params);
+		this.addContentView(Viewcount, params);    //计数器
 		Viewcount.setTextSize(Width/17);
 	    Viewcount.setX(Width*5/12);
 	    Viewcount.setTextColor(Color.RED);
 		Viewcount.setY(0);
 		
-			
-		
-		
 	}
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	@SuppressLint("NewApi")
-	public void initBlock(Button bb1,Button bb2){ 
+	
+	public void groupBlock(){
+		switch (groupIndex%12) {
+		case 0:
+			initBlock(0,1,bgroup[0], bgroup[1]);
+			groupIndex = groupIndex+2;
+			break;
+        case 2:
+			initBlock(2,3,bgroup[2], bgroup[3]);
+			groupIndex = groupIndex+2;
+			break;
+        case 4:
+			initBlock(4,5,bgroup[4], bgroup[5]);
+			groupIndex = groupIndex+2;
+			break;
+        case 6:
+			initBlock(6,7,bgroup[6], bgroup[7]);
+			groupIndex = groupIndex+2;
+			break;
+        case 8:
+			initBlock(8,9,bgroup[8], bgroup[9]);
+			groupIndex = groupIndex+2;
+			break;
+        case 10:
+			initBlock(10,11,bgroup[10], bgroup[11]);
+			groupIndex = groupIndex+2;
+			break;
+		}
+			
+	}
+	
+
+	
+	public void ActionStop(){                          //暂停所有滑块
+		for(int k=0;k<12;k++){
+			objgroup[k].cancel();
+		}
+		mp3.stop();
+		mp3.release();
+	}
+	
+	
+	
+	
+	public void initBlock(final int i,int j,Button bb1,Button bb2){ 
 		Viewcount.bringToFront();
 		bb1 = new Button(AnimationTest.this);
 		bb2 = new Button(AnimationTest.this);
@@ -112,10 +180,7 @@ public class AnimationTest extends Activity {
 		r = new Random();
 		currentcolor = r.nextInt(2);
 		
-    	bb1.setWidth(Width/2);
-        bb1.setHeight(Height/5);
-		bb1.setX(0.5f*Width);
-		bb1.setY(-500);
+    	
 		if(currentcolor==0){
 			bb1.setBackgroundColor(Color.BLACK);
 			bb2.setBackgroundColor(Color.WHITE);
@@ -123,36 +188,72 @@ public class AnimationTest extends Activity {
 		else if(currentcolor==1){
 			bb1.setBackgroundColor(Color.WHITE);
 			bb2.setBackgroundColor(Color.BLACK);
-			
 		}
 		
-		
+		bb1.setWidth(Width/2);
+        bb1.setHeight(Height/5);
+		bb1.setX(0.5f*Width);
+		bb1.setY(-500);
 		this.addContentView(bb1, params);
 		bb2.setWidth(Width/2);
         bb2.setHeight(Height/5);
 		bb2.setX(0);
 		bb2.setY(-500);
 		this.addContentView(bb2, params);
-		objleft = ObjectAnimator.ofFloat(bb1, "TranslationY", -Height/4,Height);
-		objleft.setInterpolator(new LinearInterpolator());
-		objleft.setDuration(speed/4*6);
-		objleft.start();
-		objright = ObjectAnimator.ofFloat(bb2, "TranslationY", -Height/4,Height);
-		objright.setInterpolator(new LinearInterpolator());
-		objright.setDuration(speed/4*6);
-		objright.start();
+		
+		objgroup[i] = ObjectAnimator.ofFloat(bb1, "TranslationY", -Height/4,Height);
+		objgroup[i].setInterpolator(new LinearInterpolator());
+		objgroup[i].setDuration(speed/4*6);
+		objgroup[i].start();
+		objgroup[j] = ObjectAnimator.ofFloat(bb2, "TranslationY", -Height/4,Height);
+		objgroup[j].setInterpolator(new LinearInterpolator());
+		objgroup[j].setDuration(speed/4*6);
+		objgroup[j].start();
 		AnimatorSet as = new AnimatorSet();
-		as.play(objleft).with(objright);               //同时启动
+		as.play(objgroup[i]).with(objgroup[j]);               //同时启动
 		
 		bb1.setOnClickListener(new OnClickListener() {
 			
-			@SuppressLint("NewApi")
 			@Override
 			public void onClick(View v) {
 				Drawable background = v.getBackground();
 				ColorDrawable colorDrawable = (ColorDrawable) background;
 				int color = colorDrawable.getColor();
 				int getcolor = Resources.getSystem().getColor(android.R.color.black);
+				if(i!=(isPressed%12)){
+					ActionStop();
+					AnimationTest.this.onPause();
+					AlertDialog.Builder builder = new Builder(AnimationTest.this);
+					builder.setCancelable(false);
+					builder.setMessage("最终成绩"+"\n"+count);
+					builder.setTitle("还需要多加把劲哟~");
+					
+					builder.setPositiveButton("结束", new android.content.DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							arg0.dismiss();
+			                Intent intent = new Intent(AnimationTest.this,Gallery3DActivity.class);
+			                startActivity(intent);
+			                AnimationTest.this.finish();
+							// TODO Auto-generated method stub
+							
+						}
+					});
+					builder.setNegativeButton("重来", new android.content.DialogInterface.OnClickListener(){
+						public void onClick(DialogInterface arg0, int arg1){
+							arg0.dismiss();
+							Intent intent1 = new Intent(AnimationTest.this,AnimationTest.class);
+			                startActivity(intent1);
+							count = 0;
+							Viewcount.setText("00");
+						}
+					});
+					builder.show();
+					handler.removeCallbacks(task);
+				}
+				else{
+					isPressed = isPressed+2;
 				if(color == getcolor){
 					v.setBackgroundColor(Color.GRAY);
 					if(count<9){
@@ -165,7 +266,12 @@ public class AnimationTest extends Activity {
 					}
 				}
 				else{
+					
+	
+					ActionStop();
+					AnimationTest.this.onPause();
 					AlertDialog.Builder builder = new Builder(AnimationTest.this);
+					builder.setCancelable(false);
 					builder.setMessage("最终成绩"+"\n"+count);
 					builder.setTitle("还需要多加把劲哟~");
 					
@@ -173,37 +279,70 @@ public class AnimationTest extends Activity {
 						
 						@Override
 						public void onClick(DialogInterface arg0, int arg1) {
-							
 							arg0.dismiss();
 			                Intent intent = new Intent(AnimationTest.this,Gallery3DActivity.class);
 			                startActivity(intent);
+			                AnimationTest.this.finish();
 							// TODO Auto-generated method stub
 							
 						}
 					});
 					builder.setNegativeButton("重来", new android.content.DialogInterface.OnClickListener(){
 						public void onClick(DialogInterface arg0, int arg1){
+							arg0.dismiss();
+							Intent intent1 = new Intent(AnimationTest.this,AnimationTest.class);
+			                startActivity(intent1);
 							count = 0;
 							Viewcount.setText("00");
 						}
 					});
 					builder.show();
-					
-					objleft.end();
-					objright.end();
 					handler.removeCallbacks(task);
-				}
+				}}
 			}
 		});
 		bb2.setOnClickListener(new OnClickListener() {
 			
-			@SuppressLint("NewApi")
 			@Override
 			public void onClick(View v) {
 				Drawable background = v.getBackground();
 				ColorDrawable colorDrawable = (ColorDrawable) background;
 				int color = colorDrawable.getColor();
 				int getcolor = Resources.getSystem().getColor(android.R.color.black);
+				if(i!=(isPressed%12)){
+					ActionStop();
+
+					AlertDialog.Builder builder = new Builder(AnimationTest.this);
+					builder.setCancelable(false);
+					builder.setMessage("最终成绩"+"\n"+count);
+					builder.setTitle("还需要多加把劲哟~");
+					
+					builder.setPositiveButton("结束", new android.content.DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface arg0, int arg1) {
+							arg0.dismiss();
+			                Intent intent = new Intent(AnimationTest.this,Gallery3DActivity.class);
+			                startActivity(intent);
+			                AnimationTest.this.finish();
+			              // TODO Auto-generated method stub
+							
+						}
+					});
+					builder.setNegativeButton("重来", new android.content.DialogInterface.OnClickListener(){
+						public void onClick(DialogInterface arg0, int arg1){
+							arg0.dismiss();
+							Intent intent1 = new Intent(AnimationTest.this,AnimationTest.class);
+			                startActivity(intent1);
+							count = 0;
+							Viewcount.setText("00");
+						}
+					});
+					builder.show();
+					handler.removeCallbacks(task);
+				}
+				else{
+					isPressed = isPressed+2;
 				if(color == getcolor){
 					v.setBackgroundColor(Color.GRAY);
 				
@@ -217,7 +356,10 @@ public class AnimationTest extends Activity {
 					}
 				}
 				else{
+					ActionStop();
+
 					AlertDialog.Builder builder = new Builder(AnimationTest.this);
+					builder.setCancelable(false);
 					builder.setMessage("最终成绩"+"\n"+count);
 					builder.setTitle("还需要多加把劲哟~");
 					
@@ -228,21 +370,23 @@ public class AnimationTest extends Activity {
 							arg0.dismiss();
 			                Intent intent = new Intent(AnimationTest.this,Gallery3DActivity.class);
 			                startActivity(intent);
+			                AnimationTest.this.finish();
 			              // TODO Auto-generated method stub
 							
 						}
 					});
 					builder.setNegativeButton("重来", new android.content.DialogInterface.OnClickListener(){
 						public void onClick(DialogInterface arg0, int arg1){
+							arg0.dismiss();
+							Intent intent1 = new Intent(AnimationTest.this,AnimationTest.class);
+			                startActivity(intent1);
 							count = 0;
 							Viewcount.setText("00");
 						}
 					});
 					builder.show();
-					objleft.end();
-					objright.end();
 					handler.removeCallbacks(task);
-				}
+				}}
 			}
 		});
 		
@@ -259,4 +403,3 @@ public class AnimationTest extends Activity {
 	}
 
 }
-
--- src/com/example/z2/Gallery3DActivity.java
+++ src/com/example/z2/Gallery3DActivity.java
@@ -1,23 +1,24 @@
-package com.example.z2;
+package com.example.test;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Gallery;
 import android.widget.Toast;
 
 public class Gallery3DActivity extends Activity {
-
-	int mSingleChoiceID = -1;
 	
+	int mSingleChoiceID = -1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -25,15 +26,14 @@ public class Gallery3DActivity extends Activity {
 		setContentView(R.layout.activity_gallery3_d);
 	
 	
-	 Integer[] images = { R.drawable.baby, R.drawable.single,
-             R.drawable.c, R.drawable.d, R.drawable.e,
-             R.drawable.f,  R.drawable.g,R.drawable.h };
+	 Integer[] images = { R.drawable.single3, R.drawable.double3,
+             R.drawable.baby3, R.drawable.d};
      
      ImageAdapter adapter = new ImageAdapter(this, images);
      adapter.createReflectedImages();//创建倒影效果
      Gallery galleryFlow = (Gallery) this.findViewById(R.id.Gallery01);
      galleryFlow.setFadingEdgeLength(0);
-     galleryFlow.setSpacing(800); //图片之间的间距
+     galleryFlow.setSpacing(400); //图片之间的间距
     // galleryFlow.setBackgroundResource(R.drawable.line3);
      galleryFlow.setAdapter(adapter);
      
@@ -41,31 +41,25 @@ public class Gallery3DActivity extends Activity {
          public void onItemClick(AdapterView<?> parent, View view,
                  int position, long id) {
              Toast.makeText(getApplicationContext(), String.valueOf(position), Toast.LENGTH_SHORT).show();
+             if(position==0){
+            	 Intent intent0 = new Intent(Gallery3DActivity.this,MusicList.class);
+            	 startActivity(intent0);
+             }
              if(position==1){
-            	 Intent intent1 = new Intent(Gallery3DActivity.this,AnimationTest.class);
+            	 Intent intent1 = new Intent(Gallery3DActivity.this,DoubleGame.class);
             	 startActivity(intent1);
-            	 //ETC.star =55;
-             }else if(position==0)
-             {
-            	 yjms(view);
+             }
+             if(position==2){
+            	 etcselect();
              }
          }
          
      });
      galleryFlow.setSelection(0);
  }
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.gallery3_d, menu);
-		return true;
-	}
 	
-public void yjms(View v){
-		
-		new AlertDialog.Builder(this).setTitle("请选择游戏").setSingleChoiceItems(new String[]{"数字","颜色","图片"},0,new DialogInterface.OnClickListener(){
+	public void etcselect(){
+		new AlertDialog.Builder(this).setTitle("请选择游戏").setSingleChoiceItems(new String[]{"数字","颜色+文字","图片","颜色"},0,new DialogInterface.OnClickListener(){
 			public void onClick(DialogInterface dialog,int which){
 				mSingleChoiceID = which;
 				
@@ -76,22 +70,54 @@ public void yjms(View v){
 					ETC.star=1;
 					Intent intent = new Intent(Gallery3DActivity.this,ETC.class);
 					startActivity(intent);
-					Gallery3DActivity.this.finish();
+					
 				}
 				else if(mSingleChoiceID==1){
 					ETC.star=2;
 					Intent intent = new Intent(Gallery3DActivity.this,ETC.class);
 					startActivity(intent);
-					Gallery3DActivity.this.finish();
+					
 				}
 				else if(mSingleChoiceID==2){
 					ETC.star=3;
 					Intent intent = new Intent(Gallery3DActivity.this,ETC.class);
 					startActivity(intent);
-					Gallery3DActivity.this.finish();
+					
+				}
+				else if(mSingleChoiceID==3){
+					ETC.star=4;
+					Intent intent = new Intent(Gallery3DActivity.this,ETC.class);
+					startActivity(intent);
+					
 				}
 			}}).show();
-			
+	}
+	public boolean onKeyDown(int keyCode,KeyEvent event){
+		if(keyCode==KeyEvent.KEYCODE_BACK){
+			AlertDialog.Builder isExit = new Builder(Gallery3DActivity.this);
+			isExit.setTitle("系统提示");
+			isExit.setMessage("确定要退出吗");
+			isExit.setPositiveButton("坚决要走", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					Gallery3DActivity.this.finish();
+				}
+			});
+			isExit.setNegativeButton("回心转意", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+			isExit.show();
 		}
+		return false;
+	}
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.gallery3_d, menu);
+		return true;
+	}
 
 }
